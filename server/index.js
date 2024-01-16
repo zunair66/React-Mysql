6e4404b7c0d3185e780eaec4124b028b377e25d4
zunair66 <zunairarif89@gmail.com>
2024-01-16 01:57:04 -0800

    First Commit

---
+++ server/index.js
@@ -0,0 +1,75 @@
+import express from "express";
+import mysql, { createConnection } from "mysql";
+import cors from 'cors'
+const app = express();
+app.use(express.json())
+app.use(cors())
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "zain@786#12345",
+  database: "test",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("error connecting to mysql", err.message);
+    return;
+  } else {
+    console.log("Connected to mysql");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.json("hello this is the backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `desc`,`price`, `cover`) VALUES(?)";
+  const values = [
+   req.body.title,
+   req.body.desc,
+   req.body.price,
+   req.body.cover
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been created successfully.");
+  });
+});
+
+app.delete("/books/:id", (req, res) =>{
+    const bookId = req.params.id
+    const q = "DELETE FROM books WHERE id = ?"
+
+    db.query(q, [bookId], (err, data) =>{
+        if(err) return res.json(err)
+        return res.json("Book has been deleted successfully")
+    })
+})
+
+app.put("/books/:id", (req, res) =>{
+    const bookId = req.params.id
+    const q = "UPDATE books SET `title` = ?, `desc` = ?, `price` = ? , `cover` = ? WHERE id = ?"
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.price,
+        req.body.cover
+       ];
+    db.query(q, [...values, bookId], (err, data) =>{
+        if(err) return res.json(err)
+        return res.json("Book has been updated successfully")
+    })
+})
+app.listen(8800, () => {
+  console.log("Connected to server");
+});
